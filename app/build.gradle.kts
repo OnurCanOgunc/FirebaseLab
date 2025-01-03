b88368d17af43ab34d9f19f0a98d627c25dc256c
Onur Can Öğünç <ogunc606@gmail.com>
2025-01-03 13:25:28 +0300

    Firebase Auth and Navigation Compose dependencies added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,8 @@ dependencies {
 
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.crashlytics)
+    implementation(libs.firebase.auth)
+
+    implementation(libs.androidx.navigation.compose)
+
 }
