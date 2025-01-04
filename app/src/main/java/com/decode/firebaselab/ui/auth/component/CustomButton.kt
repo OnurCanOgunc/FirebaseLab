886b5afa68452a85481073151defb4102d67fe30
Onur Can Öğünç <ogunc606@gmail.com>
2025-01-04 18:07:27 +0300

    Added CustomButton

---
+++ app/src/main/java/com/decode/firebaselab/ui/auth/component/CustomButton.kt
@@ -0,0 +1,51 @@
+package com.decode.firebaselab.ui.auth.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.decode.firebaselab.ui.theme.black2
+
+@Composable
+fun CustomButton(modifier: Modifier = Modifier, painter: Painter, title: String) {
+    Box(
+        modifier = modifier
+            .padding(top = 10.dp)
+            .fillMaxWidth()
+            .height(48.dp)
+            .background(black2)
+            .border(1.dp, Color.White ,CircleShape),
+        contentAlignment = Alignment.CenterStart
+    ) {
+        Image(
+            painter = painter,
+            contentDescription = "",
+            modifier = Modifier
+                .padding(start = 16.dp)
+                .size(16.dp)
+        )
+        Text(
+            text = title,
+            color = Color.White,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            fontSize = 18.sp
+        )
+    }
+}
